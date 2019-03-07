0cbbdd38ab8929b39c231fbeae302d7b44fe64b1
novalis111 <post@ingo-weseloh.de>
2019-03-07 09:20:29 +0100

    Configure xdebug

--- app/index.php
+++ app/index.php
@@ -1,8 +1,16 @@
-<html>
+<html lang="en">
 <head>
     <title>devbox</title>
 </head>
 <body>
-    <h1><?php echo "Your devbox is working!"; ?></h1>
+    <h1 style="text-align: center"><?php echo "Your devbox is working!"; ?></h1>
+    <?php
+        phpinfo();
+        $foo = 'bar';
+        if ($foo) {
+            # test your xdebug here
+            $foo = 'baz';
+        }
+    ?>
 </body>
 </html>
